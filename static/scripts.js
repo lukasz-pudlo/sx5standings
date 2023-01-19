dfee35ac3926867a1d386c21c89f9424a7f4fdbc
lukasz <l.pudlo1990@gmail.com>
2023-01-19 21:48:18 +0000

    add category filter

--- static/scripts.js
+++ static/scripts.js
@@ -18,3 +18,29 @@ function filterResults(searchValue) {
       $(this).toggle($(this).text().toLowerCase().indexOf(searchValue) > -1);
   });
 }
+
+// Search category
+const categorySelect = document.getElementById('category-select');
+const categoryValues = new Set();
+
+// populate the select element with unique categories
+document.querySelectorAll('td[headers="category"]').forEach(categoryTd => {
+    categoryValues.add(categoryTd.textContent);
+});
+categoryValues.forEach(category => {
+    const option = document.createElement('option');
+    option.value = category;
+    option.textContent = category;
+    categorySelect.appendChild(option);
+});
+
+categorySelect.addEventListener('change', () => {
+    const selectedCategory = categorySelect.value;
+    document.querySelectorAll('tbody tr').forEach(row => {
+        if (selectedCategory === 'all' || row.querySelector('td[headers="category"]').textContent === selectedCategory) {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+});
